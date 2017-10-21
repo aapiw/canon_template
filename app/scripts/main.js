1c8f00820df386f0a2b6208f3da3b0606acbcb9a
Yasfi Fauzie <yasfi.mail@gmail.com>
2017-10-21 15:30:00 +0700

    updated responsive

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,44 @@
-console.log('\'Allo \'Allo!');
+
+function footerCentering(){
+	var SCREEN_WIDTH = $(window).width();
+	if (SCREEN_WIDTH < 753) {
+		$("footer .top ul:last-child li span").css('margin-right', SCREEN_WIDTH/3-50+"px");
+	}else{
+		$("footer .top ul:last-child li span").removeAttr("style");
+	}
+}
+
+function showToTop(){
+	var SCREEN_WIDTH = $(window).width();
+	console.log(SCREEN_WIDTH)
+	if (SCREEN_WIDTH < 753) {
+		console.log($(window).scrollTop());
+		if ($(window).scrollTop() > 5750) {
+			$(".to-top").removeClass('hide')
+			console.log("show");
+		}else{
+			$(".to-top").addClass('hide')
+			console.log("hide");
+		}
+	}
+}
+
+$(window).resize(function() {
+	footerCentering();
+	showToTop();
+});
+
+
+$(window).scroll(function() {
+	showToTop();
+});
+
+$(document).ready(function() {
+	footerCentering();
+	showToTop()
+	$(".to-top a").click(function() {
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+		return false;
+	});
+
+});
